83db16211dcf24a2a898ea2395c81488d31ca9b5
mariebdt <106916654+mariebdt@users.noreply.github.com>
2023-04-20 21:24:14 +0200

    Add files via upload

--- scripts/viewTables.sql
+++ scripts/viewTables.sql
@@ -1,4 +1,4 @@
--- Create a view with all the absences of the responsible
+ -- Create a view with all the absences of the responsible
 CREATE OR REPLACE VIEW V_RESPONSABLE_ABSENCE AS
 SELECT * FROM ABSENCE
 WHERE id_employe IN (SELECT id_emp FROM EMPLOYE WHERE est_responsable = 1);
@@ -15,5 +15,15 @@ SELECT * FROM V_DEMANDE_CONGES;
 SELECT * FROM CONGES;
 
 -- Support hours realized during a week
+CREATE VIEW HEURESUPP_SEMAINE AS
+SELECT e.nom, e.prenom, hs.numero_semaine, SUM((hs.heure_fin - hs.heure_debut) * 24) AS heures_supplementaires
+FROM EMPLOYE e
+JOIN HEURESUPP hs ON e.id_emp = hs.id_employe
+GROUP BY e.nom, e.prenom, hs.numero_semaine;
 
 -- Notification not read
+CREATE VIEW NOTIFICATIONS_NON_LUES AS
+SELECT id_emp, COUNT(*) AS nb_notifications_non_lues
+FROM EMPLOYE e JOIN NOTIFICATION n ON e.id_emp = n.id_employe
+WHERE n.est_lue = 0
+GROUP BY id_emp;
