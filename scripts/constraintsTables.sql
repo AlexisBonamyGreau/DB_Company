99c41cffda9f1c048ae8207b02dd878e636d6244
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-20 21:39:31 +0200

    small fix

--- scripts/constraintsTables.sql
+++ scripts/constraintsTables.sql
@@ -4,5 +4,3 @@ ADD CONSTRAINT unique_rapport_hebdo UNIQUE (id_employe, numero_semaine, annee);
 -- Test the constraint
 INSERT INTO RAPPORTHEBDO VALUES (2, 2, 16, 2023, 1, 5, 1, 2, 8); -- should not work
 INSERT INTO RAPPORTHEBDO VALUES (2, 2, 17, 2023, 1, 5, 1, 2, 8); -- should work
-
--- Check if the start date is before the end date when a vacation is requested
